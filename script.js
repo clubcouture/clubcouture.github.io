dce269cdf1c42745b695f569793de16278858658
clubcouture <closmaire.clubcouture@gmail.com>
2025-05-21 15:01:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const headerLinks = document.querySelectorAll(".hyperlien");
 
+    headerLinks.forEach((link) => {
+        link.addEventListener("click", () => {
+            // Supprime la classe active de tous les liens
+            headerLinks.forEach((l) => l.classList.remove("active"));
+
+            // Ajoute la classe active au lien cliqué
+            link.classList.add("active");
+        });
+    });
+});
